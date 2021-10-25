81e1461c476f25db04b63955e5edd0427115da88
Kehinde Onifade <onifkay@gmail.com>
2021-10-25 15:31:14 +0100

    initial commit

---
+++ includes/linkbar.php
@@ -0,0 +1,51 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="stylesheet" type="text/css" href="linkbar.css" />
+
+<title>Portal</title>
+
+<style type="text/css">
+	.menubar{
+		background-color: red; width: 100%; height: 45px; margin-top: -17px;
+	}
+	.menubar ul{
+		list-style: none; padding: 10px;
+	}
+		.menubar ul li{
+			display: inline; font-size: 18px;  tex
+	}
+	.menubar ul li a{ 
+	text-decoration: none; color: white; }
+
+
+
+
+		
+</style>
+</head>
+<body>
+
+
+
+
+        <div class="menubar">
+            	<ul>
+                <li> <a href="portal.php">Home </a> </li>  |  
+                	<li><a href="update.php">Update</a></li>   | 
+                	<li> <a href="message.php">Compose SMS </a> </li>  |
+                		 <li>  <a href="inbox.php">Inbox <?php echo$n2 ?></a></li>  |  
+                		   <li> <a href="chat.php">Chat </a></li>  | 
+                		   	   <li> <a href="updatepp.php">Update Profile Picture </a></li>  |  
+                		 <li>  <a href="logout.php">Logout</a></li> 
+
+                
+                </ul>
+             
+            </div>
+          
+
+</body>
+</html>
